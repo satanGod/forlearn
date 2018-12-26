d6c941a5c6bd5479ba583edee1a916879d6c64f5
jiangxu <347635784@qq.com>
2018-12-26 16:33:59 +0800

    初始化

---
+++ src/main/java/com/learn/demo/ForLearnApplication.java
@@ -0,0 +1,14 @@
+package com.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForLearnApplication.class, args);
+    }
+
+}
+
